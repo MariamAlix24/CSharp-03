3544dc67bce06ba5c39768292fc2795ac3edfeb1
MariamAlix24 <alimariem59@gmail.com>
2026-08-30 21:50:05 +0300

    Question 3

--- G-ASP-NET-99-C#Basics-03/Program.cs
+++ G-ASP-NET-99-C#Basics-03/Program.cs
@@ -6,32 +6,36 @@
         {
             #region  Question 1
 
-            int pages = 464;
-            bool isAvailable = true;
-            if (pages > 300 && isAvailable == true)
-            Console.WriteLine("You can borrow this book");
+            /* int pages = 464;
+             bool isAvailable = true;
+             if (pages > 300 && isAvailable == true)
+             Console.WriteLine("You can borrow this book");*/
             #endregion
 
             #region Question 2
 
-            string title = "Refactoring";
-            switch (title)
-            {
-                case "Clean Code":
-                    Console.WriteLine("Great choice!");
-                    break;
-                case "Refactoring":
-                    Console.WriteLine("Nice pick!");
-                    break;
-                default:
-                    Console.WriteLine("Never heard of it.");
-                    break;
-            }
+            /* string title = "Refactoring";
+             switch (title)
+             {
+                 case "Clean Code":
+                     Console.WriteLine("Great choice!");
+                     break;
+                 case "Refactoring":
+                     Console.WriteLine("Nice pick!");
+                     break;
+                 default:
+                     Console.WriteLine("Never heard of it.");
+                     break;
+             }*/
 
+            #endregion
+            #region Question3
+            int pages = 464;
+            string sizeLabel = pages > 300 ? "Long Book" : "Small Book";
+            Console.WriteLine(sizeLabel);
             #endregion
 
 
-            
         }
     }
 }
